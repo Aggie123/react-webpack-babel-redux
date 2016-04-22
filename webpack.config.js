99f4a01e3e322488614b9e5ff94a0994fc156e62
snn <shennana@shuidao.com>
2016-04-22 17:46:29 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+
+module.exports = {
+  // entry: './js/entry.js',
+  entry: [
+    'webpack-dev-server/client?http://127.0.0.1:4001', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server',
+    './js/index.js' // Your appʼs entry point
+  ],
+  output: {
+    path: __dirname + '/build/',
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {test: /\.css$/, loader: 'style!css'},
+      {
+        test: /\.jsx?$/,
+        loaders: ['react-hot', 'babel'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+};
